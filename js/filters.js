435447cc3607f439a10cd321f696d4aedf1f4d47
combizs <combiz.s@gmail.com>
2013-11-22 16:33:21 -0800

    Cleaned up conditional statement

--- js/filters.js
+++ js/filters.js
@@ -3,7 +3,7 @@
 /* Filters */
 mediaApp.filter('capitalize', function() {
     return function(input, scope) {
-        if (input!=null)
+        if (input)
             return input.substring(0,1).toUpperCase()+input.substring(1);
     }
 });
